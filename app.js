a713748562693e134042f3a0df6b6863fe6a7976
rahul patil <rahulpatil2387@gmail.com>
2023-12-19 05:11:06 +0530

    Update validations

--- app.js
+++ app.js
@@ -43,5 +43,43 @@ document.addEventListener('DOMContentLoaded', function () {
   function validateForm() {
     // Validation logic remains the same
     // ...
-  }
-});
+      const firstName = form.elements['firstName'].value;
+      const lastName = form.elements['lastName'].value;
+      const email = form.elements['email'].value;
+      const phone = form.elements['phone'].value;
+
+      if (!isValidName(firstName)) {
+        alert('Please enter a valid first name');
+        return false;
+      }
+
+      if (!isValidName(lastName)) {
+        alert('Please enter a valid last name');
+        return false;
+      }
+
+      if (!isValidEmail(email)) {
+        alert('Please enter a valid email address');
+        return false;
+      }
+
+      if (!isValidPhone(phone)) {
+        alert('Please enter a valid 10-digit phone number');
+        return false;
+      }
+
+      return true;
+    }
+
+    function isValidName(name) {
+      return /^[A-Za-z]+$/.test(name);
+    }
+
+    function isValidEmail(email) {
+      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
+
+    function isValidPhone(phone) {
+      return /^\d{10}$/.test(phone);
+    }
+  });
